69a8ec98de7ff3038d454fdfb5801db3c3e4bc75
agostinoluca <agostino.luca89@gmail.com>
2024-04-11 21:09:18 +0200

    add header

---
+++ schema.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/layout/head.php';
+?>
+
+<main>
+    <div class="container mt-5">
+        <img src="./schema/schema.png" alt="schema of database">
+    </div>
+</main>
